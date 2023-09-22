728a7a8637ef23cd1b34c7bb9687286e21a7f2fe
Ocha-Misaki <himawari.158fuji@gmail.com>
2023-09-22 15:46:46 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+'use strict'
+{
+  // const field_vertical = 40
+  // const field_beside = 10
+  // const block_size = 30
+  // const tetro_size = 4
+  // const canvas_vertical = field_vertical * block_size
+  // const canvas_beside = field_beside * block_size
+  // let canvasId = document.getElementById('canvasId')
+  // let conText = canvasId.getContext('2d')
+  // canvasId.width = canvas_beside
+  // canvasId.height = canvas_vertical
+  // canvasId.style.border = '4px solid #555'
+  // let tetro = [
+  //   [0, 0, 0, 0],
+  //   [1, 1, 0, 0],
+  //   [0, 1, 1, 0],
+  //   [0, 0, 0, 0],
+  // ]
+  // for (let y = 0; y < tetro_size; y++) {
+  //   for (let x = 0; x < tetro_size; x++) {
+  //     if (tetro[y][x]) {
+  //       let px = x * block_size
+  //       let py = y * block_size
+  //       conText.fillStyle = 'red'
+  //       conText.fillRect(px, py, block_size, block_size)
+  //       conText.strokeStyle = 'black'
+  //       conText.strokeRect(px, py, block_size, block_size)
+  //     }
+  //   }
+  // }
+}
